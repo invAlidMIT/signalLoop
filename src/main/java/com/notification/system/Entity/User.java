a827a85c047ac7fc20d7dcbbf6550258099c54de
Amit <amithegde777@gmail.com>
2026-04-02 11:30:17 +0530

    Initial commit

---
+++ src/main/java/com/notification/system/Entity/User.java
@@ -0,0 +1,26 @@
+package com.notification.system.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String timeZone;
+    private String preferredChannel;
+
+
+}
