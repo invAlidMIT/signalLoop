7d2b417d185fa89179d22b0c52e231f61083d45d
Amit <amithegde777@gmail.com>
2026-05-24 12:20:41 +0530

    add data seed file for testing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,73 @@
+-- CLEAN TABLES
+TRUNCATE TABLE notifications RESTART IDENTITY CASCADE;
+TRUNCATE TABLE users RESTART IDENTITY CASCADE;
+TRUNCATE TABLE urgency_weight RESTART IDENTITY CASCADE;
+TRUNCATE TABLE reliability RESTART IDENTITY CASCADE;
+TRUNCATE TABLE factor_percentage RESTART IDENTITY CASCADE;
+
+
+-- FACTOR PERCENTAGES
+INSERT INTO factor_percentage (factor, percentage)
+VALUES
+('USER_PREFERENCE', 40),
+('RELIABILITY', 35),
+('URGENCY', 25);
+
+
+-- RELIABILITY SCORES
+INSERT INTO reliability (channel, value)
+VALUES
+('SMS', 95),
+('EMAIL', 80),
+('PUSH', 70);
+
+
+-- URGENCY WEIGHTS
+INSERT INTO urgency_weight (urgency, channel, urgency_percentage)
+VALUES
+('HIGH','PUSH',100),
+('HIGH','SMS',85),
+('HIGH','EMAIL',50),
+
+('MEDIUM','PUSH',75),
+('MEDIUM','SMS',85),
+('MEDIUM','EMAIL',80),
+
+('LOW','PUSH',60),
+('LOW','SMS',70),
+('LOW','EMAIL',100);
+
+
+-- USERS
+INSERT INTO users
+(email, password, timezone, preferred_channel, created_at, role)
+VALUES
+(
+'xys@gmail.com',
+'$2a$10$bQMz20Kr.bnG2SK8I124N.IPg/cnJEimGE9U660FBxwp6IAAbS5wS',
+'Asia/Kolkata',
+'EMAIL',
+NOW(),
+'ROLE_ADMIN'
+);
+
+
+-- NOTIFICATIONS
+INSERT INTO notifications
+(user_id, notification_status, retry_count, created_at, channel, message, urgency)
+VALUES
+
+-- HIGH
+((SELECT id FROM users WHERE email='xys@gmail.com),'PENDING',0,NOW(),'PUSH','Server CPU usage critical','HIGH'),
+((SELECT id FROM users WHERE email='xys@gmail.com'),'PENDING',0,NOW(),'SMS','Payment failed for customer','HIGH'),
+((SELECT id FROM users WHERE email='xys@gmail.com'),'PENDING',0,NOW(),'EMAIL','Security alert login attempt','HIGH'),
+
+-- MEDIUM
+((SELECT id FROM users WHERE email='xys@gmail.com'),'PENDING',0,NOW(),'EMAIL','Daily backup completed','MEDIUM'),
+((SELECT id FROM users WHERE email='xys@gmail.com'),'PENDING',0,NOW(),'SMS','Invoice generated successfully','MEDIUM'),
+((SELECT id FROM users WHERE email='xys@gmail.com'),'PENDING',0,NOW(),'PUSH','Weekly report ready','MEDIUM'),
+
+-- LOW
+((SELECT id FROM users WHERE email='xys@gmail.com'),'PENDING',0,NOW(),'EMAIL','Promotional offer available','LOW'),
+((SELECT id FROM users WHERE email='xys@gmail.com'),'PENDING',0,NOW(),'SMS','Newsletter published','LOW'),
+((SELECT id FROM users WHERE email='xys@gmail.com'),'PENDING',0,NOW(),'PUSH','App update available','LOW');
