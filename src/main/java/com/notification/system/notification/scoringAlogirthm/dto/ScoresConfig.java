9fc3de8fd7623c8481b178f527b7eff2b8832cc0
Amit <amithegde777@gmail.com>
2026-06-07 12:19:06 +0530

    cleanup reliability config

--- src/main/java/com/notification/system/notification/scoringAlogirthm/dto/ScoresConfig.java
+++ src/main/java/com/notification/system/notification/scoringAlogirthm/dto/ScoresConfig.java
@@ -12,7 +12,6 @@ import java.util.Map;
 public class ScoresConfig {
 
     private Map<Factor,Double> factorWeights;
-    private Map<Channel,Double> reliabilityScores;
     private Map<String,Double> UrgencyWeights;
 
 }
--- src/main/java/com/notification/system/notification/scoringAlogirthm/service/ScoringConfigService.java
+++ src/main/java/com/notification/system/notification/scoringAlogirthm/service/ScoringConfigService.java
@@ -2,13 +2,10 @@ package com.notification.system.notification.scoringAlogirthm.service;
 
 import com.notification.system.notification.scoringAlogirthm.dto.ScoresConfig;
 import com.notification.system.notification.scoringAlogirthm.entity.FactorPercentage;
-import com.notification.system.notification.scoringAlogirthm.entity.Reliability;
 import com.notification.system.notification.scoringAlogirthm.entity.UrgencyWeight;
 import com.notification.system.notification.scoringAlogirthm.enums.Factor;
 import com.notification.system.notification.scoringAlogirthm.repository.FactorPercentageRepository;
-import com.notification.system.notification.scoringAlogirthm.repository.ReliabilityRepository;
 import com.notification.system.notification.scoringAlogirthm.repository.UrgencyWeightRepository;
-import com.notification.system.user.enums.Channel;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +17,6 @@ import java.util.stream.Collectors;
 public class ScoringConfigService {
 
     private final FactorPercentageRepository factorPercentageRepository;
-    private final ReliabilityRepository reliabilityRepository;
     private final UrgencyWeightRepository urgencyWeightRepository;
 
     public ScoresConfig loadScores(){
@@ -29,13 +25,6 @@ public class ScoringConfigService {
                 .collect(Collectors.toMap(
                         FactorPercentage::getFactor,
                         FactorPercentage::getPercentage
-                ));
-
-        Map<Channel,Double> reliabilityScores=reliabilityRepository.findAll()
-                .stream()
-                .collect(Collectors.toMap(
-                        Reliability::getChannel,
-                        Reliability::getValue
                 ));
 
         Map<String,Double> urgencyWeights=urgencyWeightRepository.findAll()
@@ -47,7 +36,6 @@ public class ScoringConfigService {
 
         return new ScoresConfig(
                 factorWeights,
-                reliabilityScores,
                 urgencyWeights
         );
     }
