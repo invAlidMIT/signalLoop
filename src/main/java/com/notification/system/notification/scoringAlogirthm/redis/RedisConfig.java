7b3c568653fba256e7fa99a4ba55030b29adf1b7
Amit <amithegde777@gmail.com>
2026-06-21 11:09:13 +0530

    add cache invalidation

--- src/main/java/com/notification/system/notification/scoringAlogirthm/redis/RedisConfig.java
+++ src/main/java/com/notification/system/notification/scoringAlogirthm/redis/RedisConfig.java
@@ -1,28 +1,9 @@
 package com.notification.system.notification.scoringAlogirthm.redis;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-
-        template.setConnectionFactory(connectionFactory);
-
-        template.setKeySerializer(RedisSerializer.string());
-
-        template.setValueSerializer(RedisSerializer.json());
-
-        template.setHashKeySerializer(RedisSerializer.string());
-
-        template.setHashValueSerializer(RedisSerializer.json());
-
-        return template;
-    }
 }
