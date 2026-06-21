67b7e41c1fa71e1a096df1beba5421c8351ed8c8
Amit <amithegde777@gmail.com>
2026-06-21 11:08:29 +0530

    add factor percentage repo

--- src/main/java/com/notification/system/notification/scoringAlogirthm/repository/FactorPercentageRepository.java
+++ src/main/java/com/notification/system/notification/scoringAlogirthm/repository/FactorPercentageRepository.java
@@ -5,6 +5,9 @@ import com.notification.system.notification.scoringAlogirthm.enums.Factor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FactorPercentageRepository extends JpaRepository<FactorPercentage,Long> {
+    public Optional<FactorPercentage> findByFactor(Factor factor);
 }
