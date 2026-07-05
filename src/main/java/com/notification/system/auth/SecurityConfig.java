30482d9bdb5f9d67b52fbb3c92f5bba8adeba585
Amit <amithegde777@gmail.com>
2026-07-05 16:55:34 +0530

    feat: add actuator info

--- src/main/java/com/notification/system/auth/SecurityConfig.java
+++ src/main/java/com/notification/system/auth/SecurityConfig.java
@@ -34,7 +34,9 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "/api-docs/**").permitAll()
+                                "/api-docs/**",
+                                "/actuator/**")
+                        .permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
